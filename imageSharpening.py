18a02e37b155d880162974c7e7d12e7180bc52e0
pebblewebble <142425284+pebblewebble@users.noreply.github.com>
2024-10-07 09:21:42 +0800

    iamge sharpening

---
+++ imageSharpening.py
@@ -0,0 +1,13 @@
+from PIL import Image, ImageFilter
+
+def sharpen_image(image_path):
+    original_image=Image.open(image_path)
+
+    sharpened_image=original_image.filter(ImageFilter.SHARPEN)
+
+    original_image.show(title='Original Image')
+    sharpened_image.show(title='Sharpened Image')
+
+image_path='slightBlur.jpg'
+
+sharpen_image(image_path)
