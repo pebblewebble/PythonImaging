9f3070d36487617c2725b886e20dfd8a179ed7ad
pebblewebble <142425284+pebblewebble@users.noreply.github.com>
2024-10-07 09:57:01 +0800

    image sharpening

--- imageSharpening.py
+++ imageSharpening.py
@@ -10,4 +10,18 @@ def sharpen_image(image_path):
 
 image_path='slightBlur.jpg'
 
-sharpen_image(image_path)
+# sharpen_image(image_path)
+
+import cv2
+
+import numpy as np
+
+image=cv2.imread('slightBlur.jpg')
+kernel=np.array([[-1,-1,-1],
+                 [-1,9,-1],
+                 [-1,-1,-1]])
+sharpened=cv2.filter2D(image,-1,kernel)
+cv2.imshow('Image sharpening', sharpened)
+cv2.imshow('Original Image',image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
